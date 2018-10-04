6df581f684f6aeca8c5c062ec56bae51a5be83d8
Georgi <georgi.Babouchkine@metropolia.fi>
2018-10-04 14:37:16 +0300

    code skeleton prototype (spooky)

---
+++ Happy_Plant/Happy_Plant.ino
@@ -0,0 +1,31 @@
+//final code
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+float heatSensor() {
+
+}
+
+int soilSensor() {
+
+}
+
+void save() {
+
+}
+void printing() {
+
+}
+
+void  drawScreen(byte buffer2[]) {
+
+}
+
+
